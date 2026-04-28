fa2c09e588bd3ad3a273e2005f8da2d9ac555f8f
TheBitBanger <the-bit-banger@protonmail.com>
2026-04-27 19:17:53 -0600

    chore(tracker): remove unused django admin module

--- src/tracker/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Item
-
-
-@admin.register(Item)
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ("title", "media_type", "created_at", "updated_at")
-    search_fields = ("title", "media_type")
